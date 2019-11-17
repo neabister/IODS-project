a104b08c4909dec89390dc5f677e85a7f4278ae9
neabister <nea.bister@gmail.com>
2019-11-17 14:23:44 +0200

    Chapter3 data wrangling

--- data/create_alc.R
+++ data/create_alc.R
@@ -10,6 +10,8 @@ por <- read.csv('data/student-por.csv', sep=';')
 str(math)
 str(por)
 
+head(math)
+head(por)
 dim(math)
 dim(por)
 
@@ -32,6 +34,51 @@ alc <- select(joined_data, one_of(identifiers))
 dim(alc)
 str(alc)
 
+#Columns not used for joining (now duplicate columns  in joined_data coming from both por and math data sets)
 
+not_joined <- colnames(math)[!colnames(math) %in% identifiers]
+not_joined
+
+#Combine duplicate columns from math and por datasets to have only one value for each variable, if numeric take average, if string, take first
+
+for(column_name in  not_joined) {
+  #make a vector of vectors having same initial name e.g. math
+  columns <- select(joined_data, starts_with(column_name))
+  #select first column to test if it is numeric, if yes, take rowMean and round it, if not (else), take first value (from math dataset)
+  #append values to alc df that only had columns that were used for inner joining
+  first_column <- select(columns, 1)[[1]]
+  if(is.numeric(first_column)) {
+    alc[column_name] <- round(rowMeans(columns))
+  } else {
+    alc[column_name] <- first_column
+  }
+}
+
+glimpse(alc)
+dim(alc)
+
+#Dalc = weekday consumption, Walc = weekend consumption, add column (alc_use) of average of Dalc and Walc 
+
+alc <- mutate(alc, alc_use = (Dalc + Walc) / 2)
+dim(alc)
+
+#high_use TRUE if alc_use > 2, FALSE if <= 2
+
+alc <- mutate(alc, high_use = alc_use > 2)
+
+#Glimpse to check everything is correct (compare to the final dataframe provided)
+glimpse(alc)
 
 ready_data <- read.csv('http://s3.amazonaws.com/assets.datacamp.com/production/course_2218/datasets/alc.txt', sep=',')
+dim(ready_data)
+glimpse(ready_data)
+
+#Save the final df to .csv
+
+write.csv(alc, 'data/create_alc.csv', row.names = FALSE)
+
+#Test read
+
+test <- read.csv('data/create_alc.csv')
+head(test)
+dim(test)
